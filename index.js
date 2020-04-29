c916a854f38132e599f4084f158e95bebe0a495b
Michael Bade <mbade314@gmail.com>
2020-04-29 19:32:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,16 @@ function add2(n) {
   // Feel free to move things around!
 }
 
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
+
+
+var theFunk = funkyFunction()
+
+theFunk();
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
